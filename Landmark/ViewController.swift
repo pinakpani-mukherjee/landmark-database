77d5183496fcd889035e54af7dfa5bd48d8bff13
Pinakpani Mukherjee <mukherjee.pinakpani14@gmail.com>
2021-12-03 21:48:06 +0900

    added assets and basic tableview structure

--- Landmark/ViewController.swift
+++ Landmark/ViewController.swift
@@ -7,13 +7,28 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource {
 
+    @IBOutlet weak var tableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        tableView.delegate = self
+        tableView.dataSource = self
+        var landmarkNames =  [String]()
+        landmarkNames.append("Colosseum")
+        landmarkNames.append("Kremlin")
+        landmarkNames.append("Taj Mahal")
+        landmarkNames.append("Stonehenge")
+        landmarkNames.append("Great Wall of China")
     }
-
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel? .text = "Hellloooooo"
+        return cell
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
 
 }
 
